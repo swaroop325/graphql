9677579b601c5aad7ddf1d46d6cbf36e765ed56a
swaroop_a.s <swaroopanand325@gmail.com>
2022-02-01 13:41:37 +0530

    complex resolvers

--- resolver.js
+++ resolver.js
@@ -3,6 +3,16 @@ const { welcomeMessage, empData } = require("./emp.data");
 const resolver = {
   Query: {
     employee: () => empData,
+    employeeById: (parent, args, context, info) => {
+      return [empData.find((i) => i.id == args.id)];
+    },
+    employeeByName: (parent, args, context, info) => {
+      return [
+        empData.find(
+          (i) => i.firstName.toLowerCase() == args.name.toLowerCase()
+        ),
+      ];
+    },
     message: () => welcomeMessage,
   },
 };
--- schema.js
+++ schema.js
@@ -10,6 +10,8 @@ const typeDefs = gql`
 
   type Query {
     employee: [Employee!]!
+    employeeById(id: ID!): [Employee]
+    employeeByName(name: String): [Employee]
     message: String!
   }
 `;
