4a22561658f0958f9b7077c3dc681474f801bc23
swaroop_a.s <swaroopanand325@gmail.com>
2022-02-03 01:09:01 +0530

    client connection finishes

--- apollo-client/src/App.js
+++ apollo-client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MSG = gql`
+  query Sample {
+    message
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_MSG);
+  console.table(data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+  return <h1>{data.message}</h1>;
 }
 
 export default App;
--- apollo-client/src/index.js
+++ apollo-client/src/index.js
@@ -3,27 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  ApolloClient,
-  gql,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      query GetHelloMessage {
-        message
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+// client
+//   .query({
+//     query: gql`
+//       query GetHelloMessage {
+//         message
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
