8c6a646942759f63530b0c4e2078faba9aedd4a2
swaroop_a.s <swaroopanand325@gmail.com>
2022-02-03 14:00:56 +0530

    added refetch

--- apollo-client/src/components/Employees.js
+++ apollo-client/src/components/Employees.js
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, NetworkStatus, useQuery } from "@apollo/client";
 import React, { useState } from "react";
 import EmpCard from "./EmpCard";
 import EmployeeDetails from "./EmployeeDetails";
@@ -16,10 +16,18 @@ const GET_EMPLOYEES = gql`
 
 export default function Employees() {
   const [selected, setSelected] = useState("");
-  const { loading, error, data } = useQuery(GET_EMPLOYEES, {
-    pollInterval: 500,
-  });
+  const { loading, error, data, refetch, networkStatus } = useQuery(
+    GET_EMPLOYEES,
+    {
+      pollInterval: 0,
+      notifyOnNetworkStatusChange: true,
+    }
+  );
   console.log(data);
+  console.log("networkStatus : " + networkStatus);
+  if (networkStatus === NetworkStatus.refetch) {
+    return <b className="text-success">Refetching...</b>;
+  }
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
 
@@ -38,6 +46,9 @@ export default function Employees() {
             />
           ))}
       </div>
+      <button className="btn btn-outline-success m-2" onClick={() => refetch()}>
+        Refetch
+      </button>
       <hr />
       {selected && <EmployeeDetails empId={selected} />}
     </>
